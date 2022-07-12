5dae28633c2d3359b26c5b33df2dc3b768a351ff
cathycc68 <75711003+cathycc68@users.noreply.github.com>
2022-07-12 11:31:48 -0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("newTaskForm").addEventListener("submit", function() {
-  
-console.log('Working!');
-event.preventDefault();
-    newTaskVar.addTask();
-    newTaskVar.render();
-});
