8eca1a98879ba4d5a77ce7de3a8cb15f19ffffdf
Cat <cathycc68@gmail.com>
2022-07-15 09:45:04 -0700

    task 8

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -89,26 +89,27 @@ class TaskManager {
                 }
               }
             }
-      }
-save() {
-    var tasksJson = JSON.stringify(this.tasks);
-    localStorage.setItem('tasks', tasksJson);
-    var currentId = String(this.currentId);
-    localStorage.setItem('currentId', currentId);
-}
-
-    load() {
-      if (localStorage.getItem('tasks')) {
-          const tasksJson = localStorage.getItem('tasks');
-          this.tasks = JSON.parse(tasksJson);
-      }
-
-      if (localStorage.getItem('currentId')) {
-          const currentId = localStorage.getItem('currentId');
-          this.currentId = Number(currentId);
+            save() {
+              var tasksJson = JSON.stringify(this.tasks);
+              localStorage.setItem('tasks', tasksJson);
+              var currentId = String(this.currentId);
+              localStorage.setItem('currentId', currentId);
+          }
           
-        
+          load() {
+            if (localStorage.getItem('tasks')) {
+                const tasksJson = localStorage.getItem('tasks');
+                this.tasks = JSON.parse(tasksJson);
+             }
+          
+            if (localStorage.getItem('currentId')) {
+                const currentId = localStorage.getItem('currentId');
+                this.currentId = Number(currentId);
+            }
+          }
+      };
 
-        
+var newTaskVar = new TaskManager();
 
-
+newTaskVar.load();
+newTaskVar.render();
